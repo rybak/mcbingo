65a44cab4b9d90aa26591c15f2f4d581b1a40720
Andrei Rybak <rybak.a.v@gmail.com>
2021-03-10 13:29:06 +0100

    tests: introduce function forAllSeedsAndDifficulties

--- tests.js
+++ tests.js
@@ -29,45 +29,28 @@ function findReallyLatestVersion()
 
 function shortTest()
 {
-	let version = findReallyLatestVersion();
-	console.log("Testing version:");
-	console.log(version);
-	let listOfGoalsByDifficulty = version.goals;
 	let goalsCounters = {};
-	debugLog("List of all goals:");
-	for (var difficulty = 0; difficulty < listOfGoalsByDifficulty.length; difficulty++)
-	{
-		debugLog("  Difficulty: " + difficulty);
-		for (const goal of listOfGoalsByDifficulty[difficulty])
-		{
-			debugLog("    " + goal.name);
-			goalsCounters[goal.name] = 0;
-		}
-	}
-
 	let allFound = false;
-	for (let seed = 10000; seed < 100000; seed++)
-	{
-		for (let difficulty = 0; difficulty < listOfGoalsByDifficulty.length; difficulty++)
-		{
-			let urlDifficulty = difficulty + 1;
-			Math.seedrandom(seed.toString()); // must seed the random before every generation
-			let result = version.generator("random", urlDifficulty, listOfGoalsByDifficulty);
+	forAllSeedsAndDifficulties(
+		goal => {
+			goalsCounters[goal.name] = 0;
+		},
+		result => {
 			for (const goal of result)
 			{
 				goalsCounters[goal.name]++;
 			}
+		},
+		(seed) => {
+			allFound = Object.values(goalsCounters).every(counter => counter > 0);
+			if (allFound)
+			{
+				debugAlert("Found all goals. Had to check up to seed " + seed + ".");
+				return true;
+			}
+			return false;
 		}
-		if (seed % 10000 == 0) {
-			debugLog("CHECKED UP TO SEED = " + seed);
-		}
-		allFound = Object.values(goalsCounters).every(counter => counter > 0);
-		if (allFound)
-		{
-			debugAlert("Found all goals. Had to check up to seed " + seed + ".");
-			break;
-		}
-	}
+	);
 	if (!allFound)
 	{
 		debugAlert("Could not find some of the goals");
@@ -104,7 +87,42 @@ function shortTest()
 		}
 		debugLog("Most common goal: \"" + maxName + "\" = " + max);
 	}
+}
 
+function forAllSeedsAndDifficulties(prepGoal, sheetConsumer, stopCondition)
+{
+	let version = findReallyLatestVersion();
+	debugLog("Testing version:");
+	debugLog(version.toString());
+	let listOfGoalsByDifficulty = version.goals;
+	debugLog("List of all goals:");
+	for (var difficulty = 0; difficulty < listOfGoalsByDifficulty.length; difficulty++)
+	{
+		debugLog("  Difficulty: " + difficulty);
+		for (const goal of listOfGoalsByDifficulty[difficulty])
+		{
+			debugLog("    " + goal.name);
+			prepGoal(goal);
+		}
+	}
+
+	let allFound = false;
+	for (let seed = 10000; seed < 100000; seed++)
+	{
+		for (let difficulty = 0; difficulty < listOfGoalsByDifficulty.length; difficulty++)
+		{
+			let urlDifficulty = difficulty + 1;
+			Math.seedrandom(seed.toString()); // must seed the random before every generation
+			let result = version.generator("random", urlDifficulty, listOfGoalsByDifficulty);
+			sheetConsumer(result);
+		}
+		if (seed % 10000 == 0) {
+			debugLog("CHECKED UP TO SEED = " + seed);
+		}
+		if (stopCondition(seed)) {
+			break;
+		}
+	}
 }
 
 function getGoalNameWithoutRanges(rawGoalName)
